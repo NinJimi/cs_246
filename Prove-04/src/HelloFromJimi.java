b49ad8790e03bbb398fc810d3009e7d48b4726bf
NinJimi <77086352+NinJimi@users.noreply.github.com>
2021-05-15 13:09:29 -0700

    added another line

--- Prove-04/src/HelloFromJimi.java
+++ Prove-04/src/HelloFromJimi.java
@@ -1,5 +1,6 @@
 public class HelloFromJimi {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jimi.");
+        System.out.println("Hello, my name is also Jimi, because I can only work on assignments really late at night when everyone else is asleep.");
     }
 }
